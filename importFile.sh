4db584b9d1c40d3771491eddb37c35c73f34fd2a
dieplian <lian.diep.1@ens.etsmtl.ca>
2024-11-04 08:57:13 -0500

    add script

--- importFile.sh
+++ importFile.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Create the directory
-sudo mkdir -p /Dictionnaire
+sudo mkdir  /Dictionnaire
 
-# Change to the directory
-cd /Dictionnaire || { echo "Failed to change directory"; exit 1; }
+cd /Dictionnaire
 
-# Download the file
 sudo wget https://raw.githubusercontent.com/dieplian/Dictionnaire/main/dictionnaire.txt
